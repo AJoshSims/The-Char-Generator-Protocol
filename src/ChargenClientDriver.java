b684e92de398b116d02d7d6599cf51d5f81b0861
Evan Arroyo <Evan Arroyo>
2016-10-24 21:23:10 -0400

    code

--- src/ChargenClientDriver.java
+++ src/ChargenClientDriver.java
@@ -9,7 +9,44 @@
  */
 
 public class ChargenClientDriver {
+
     public static void main(String args[]){
-        System.out.println("Hello");
+
+        int port = 19;
+
+        String host = "polaris.cs.wcu.edu";
+
+        String client = "";
+
+        String flag = "";
+
+        try{
+            if(args.length == 1){
+                System.out.println("Invalid Number Of Arguments.");
+                System.out.println("Usage: <TCP/UDP> <host> (<port>) (<flag>)");
+                System.exit(1);
+            }
+
+            if(args.length == 2){
+                client = args[0];
+                host = args[1];
+            }
+
+            if(args.length == 3){
+                client = args[0];
+                host = args[1];
+                port = Integer.parseInt(args[2]);
+            }
+
+            if(args.length == 4){
+                client = args[0];
+                host = args[1];
+                port = Integer.parseInt(args[2]);
+                flag = args[3];
+            }
+        }catch(NumberFormatException e){
+            System.out.println("Usage: <TCP/UDP> <host> (<port>) (<flag>)");
+        }
+
     }
 }
